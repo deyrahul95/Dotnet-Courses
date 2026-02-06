ded36f6b86655fbbcb5961c3bba531df520520a0
Rahul Dey <rahuldey9623@gmail.com>
2026-02-06 11:05:14 +0000

    Added Keyed Service implementation in purchase

--- ConditionalResolve/Purchase.cs
+++ ConditionalResolve/Purchase.cs
@@ -1,13 +1,14 @@
 using ConditionalResolve.Enums;
 using ConditionalResolve.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace ConditionalResolve;
 
-public class Purchase(Func<Regions, ITaxCalculator> accessor)
+public class Purchase(IServiceProvider provider)
 {
     public void Checkout(Regions region, double total)
     {
-        var taxCalculator = accessor(region);
+        var taxCalculator = provider.GetRequiredKeyedService<ITaxCalculator>(region);
         var tax = total * taxCalculator.Calculate() / 100;
         var payment = Math.Round(total + tax, 2);
         Console.WriteLine($"Payment made: {payment} rupees");
